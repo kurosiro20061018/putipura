261753fb6114820c0e1eef4f488ed3368bab37ef
kurosiro20061018 <151811929+kurosiro20061018@users.noreply.github.com>
2024-04-25 11:21:06 +0000

    退会機能完了

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -2,10 +2,11 @@
 
 class User::SessionsController < Devise::SessionsController
   before_action :configure_sign_in_params, only: [:create]
+  before_action :reject_end_user, only: [:create]
   def after_sign_in_path_for(resource)
     user_items_path
   end
- 
+
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -27,25 +28,26 @@ class User::SessionsController < Devise::SessionsController
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :email])
   end
-  
+
   private
   # 退会しているかいないかを判断するメソッド
   def reject_end_user
   # 入力されたemailからアカウントを1件取得
-  　@end_user = User.find_by(email: params[:end_user][:email])
-  　if @end_user
-  　#有効なパスワードであるが、退会済みである場合
-      if @end_user.valid_password?(params[:end_user][:password]) && (@end_user.is_deleted == true)
+    @end_user = User.find_by(email: params[:user][:email])
+
+    if @end_user
+      #有効なパスワードであるが、退会済みである場合
+      if @end_user.valid_password?(params[:user][:password]) && (@end_user.is_deleted == true)
+
         flash[:notice] = "退会済みです。再度ご登録をしてご利用ください"
-        redirect_to new_user_session_path
-    #入力ミスによるユーザーが見つからない場合
+        redirect_to new_user_registration_path
+      #入力ミスによるユーザーが見つからない場合
       else
         flash[:notice] = "項目を再入力してください"
       end
-    #登録がまだな場合
     else
+      #登録がまだな場合
       flash[:notice] = "該当するユーザーが見つかりません。新規登録をお願いします。"
     end
   end
 end
-
