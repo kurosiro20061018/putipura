cde3fa0bcddb6761cbca904d36f16320255c3b42
kurosiro20061018 <151811929+kurosiro20061018@users.noreply.github.com>
2024-06-30 09:48:09 +0000

    fix migration files evaluation

--- db/migrate/20240413045259_create_evaluations.rb
+++ db/migrate/20240413045259_create_evaluations.rb
@@ -4,8 +4,8 @@ class CreateEvaluations < ActiveRecord::Migration[6.1]
       t.integer :item_id
       t.integer :user_id
       t.text :content
-      
-      t.timestamp
+
+      t.timestamps
     end
   end
 end
